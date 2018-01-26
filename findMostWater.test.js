e9bdcbc05a6541b37c81dfecc1e46334a01fdce8
Lisa Gee <zhusufeng@gmail.com>
2018-01-25 16:02:52 -0800

    Add new test to test findMostWater

--- findMostWater.test.js
+++ findMostWater.test.js
@@ -2,4 +2,6 @@ const findMostWater = require('./findMostWater');
 
 test('findMostWater works', () => {
   expect(findMostWater([5, 3, 7, 2, 6, 4, 5, 9, 1, 2])).toEqual([3, 8, 11]);
+  expect(findMostWater([5, 1, 2, 3, 4, 3])).toEqual([1, 5, 6]);
+
 });
