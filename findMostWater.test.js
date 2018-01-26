fb4e290defd57a6a693476e6fba5a67c66ce893d
Lisa Gee <zhusufeng@gmail.com>
2018-01-25 16:32:21 -0800

    Separate types of tests for findMostWater

--- findMostWater.test.js
+++ findMostWater.test.js
@@ -1,9 +1,15 @@
 const findMostWater = require('./findMostWater');
 
-test('findMostWater works', () => {
+test('findMostWater works when right wall is higher', () => {
   expect(findMostWater([5, 3, 7, 2, 6, 4, 5, 9, 1, 2])).toEqual([3, 8, 11]);
-  expect(findMostWater([5, 1, 2, 3, 4, 3])).toEqual([1, 5, 6]); // not hitting 4
   expect(findMostWater([2, 1, 3, 4, 5])).toEqual([1, 3, 1]); 
+});
+
+test('findMostWater works when left wall is higher', () => {
+  // expect(findMostWater([5, 1, 2, 3, 4, 3])).toEqual([1, 5, 6]); // not hitting 4
+});
+
+test('findMostWater works when there is no wall', () => {
   expect(findMostWater([5, 4, 3, 2, 1])).toEqual([]); 
   expect(findMostWater([1])).toEqual([]); 
   expect(findMostWater([1, 2])).toEqual([]); 
