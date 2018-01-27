ffeb47596cf8a3f9630bdd55ab4b50f56ffd2589
Lisa Gee <zhusufeng@gmail.com>
2018-01-26 17:19:06 -0800

    Add pseudocode to render gray chart in renderChart

---
+++ client/view/renderChart.js
@@ -0,0 +1,30 @@
+const tableContainer = document.getElementById('table-container');
+
+const renderChart = (input, output) => {
+  while (tableContainer.firstChild) {
+      tableContainer.removeChild(tableContainer.firstChild);
+  }
+
+
+  // Find max number (maxHeight) in input
+
+  // Add a table
+  // Add one tr
+  // For length of input (i)
+    // Add td (with display: flex)
+      // for length of maxHeight (j)
+        // Add blocks
+        // if input[i] (6) >= j
+          // Add class gray
+         
+
+
+
+  // Create rows of maxHeight + 1
+  // Each row has an id starting with maxHeight + 1 and decrement
+  // Going row by row, using the id
+    // 
+
+  // when output is []
+
+};
