3ed8de01df138afcc8a1fabcd25766c6afaf39ff
Lisa Gee <zhusufeng@gmail.com>
2018-01-26 17:21:10 -0800

    Remove cleanUpInput call from processInput on server side

--- server/actions/processInput.js
+++ server/actions/processInput.js
@@ -2,17 +2,8 @@ const findMostWater = require('../logic/findMostWater');
 
 const processInput = (req, res) => {
   console.log(req.body.input);
-  const heights = cleanUpInput(req.body.input);
-
-  const answer = { output: findMostWater(heights) };
+  const answer = { output: findMostWater(req.body.input) };
   res.status(201).send(answer);
-};
-
-const cleanUpInput = (string) => {
-  const clean = string.split(',');
-  return clean.map(chunk => {
-    return parseInt(chunk);
-  });
 };
 
 module.exports = processInput;
