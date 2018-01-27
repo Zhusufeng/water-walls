22ce30d1fd598cbfa5c455871090d035eb5578c0
Lisa Gee <zhusufeng@gmail.com>
2018-01-26 16:48:11 -0800

    Grab input from form & send it via fetch to the backend in postData

--- client/actions/postData.js
+++ client/actions/postData.js
@@ -1,20 +1,21 @@
-const postData = () => {
-  let testInput = '1, 5, 3, 5, 1';
-  let testObj = {
-    input: testInput
-  };
+const postData = (e) => {
+  e.preventDefault();
+
+  const input = document.getElementById('heights-form-input').value;
+  console.log(input);
 
   return fetch('/render', {
     method: 'POST',
     headers: new Headers({
       'Content-Type': 'application/json'
     }),
-    body: JSON.stringify(testObj)
+    body: JSON.stringify({input})
   })
   .then(response => {
     return response.json();
   })
   .then(data => {
     console.log(data);
-  })
+    heightsForm.reset();
+  });
 };
