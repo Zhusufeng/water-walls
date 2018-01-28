395aab7c1b61c745c68c2ed75253fb07b38b565d
Lisa Gee <zhusufeng@gmail.com>
2018-01-28 14:42:22 -0800

    Start calculating blue blocks to renderChart

--- client/view/renderChart.js
+++ client/view/renderChart.js
@@ -36,9 +36,23 @@ const renderChart = (input, output) => {
   // input is [1, 5, 3, 5, 1]
   // output is 
     // 1: [1, 2, 0] 
-    // 2: [2, 4, 2] If output[key][2] > 0, add blue
+    // 2: [2, 4, 2] If output[key][2] > 0, add blue (after any input[i])
     // 4: [4, 5, 0]
     // most: [2, 4, 2] If key === most, output[key][0] and output[key][1], black blocks
+
+  // Add blue blocks
+  // Iterate through hash
+  // If output[key][2] > 0, add blue (after any input[i])
+    // Iterate through hash[key]
+    // hash[key][0] = leftWall -1
+    // hash[key][1] = rightWall-1
+
+  for (let key in output) {
+    let shorterWall = (input[leftWall] < input[rightWall]) ? leftWall : rightWall;
+    for (let i = output[key][0] - 1; i < output[key][1]; i++) {
+      let numOfBlueBlocks = input[shorterWall] - input[i];
+    }
+  }
 
 
 
