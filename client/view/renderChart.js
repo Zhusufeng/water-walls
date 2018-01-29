f6553d4ada33910c7acef607c260ccb2117e0d13
Lisa Gee <zhusufeng@gmail.com>
2018-01-28 18:26:45 -0800

    Refine renderChart blue block algorithm a bit

--- client/view/renderChart.js
+++ client/view/renderChart.js
@@ -5,7 +5,7 @@ const renderChart = (input, output) => {
       tableContainer.removeChild(tableContainer.firstChild);
   }
 
-  // if input is empty...
+  // if input is []
 
   const maxHeight = findMaxHeight(input) + 1;
   console.log('This is height of our chart', maxHeight);
@@ -35,19 +35,6 @@ const renderChart = (input, output) => {
 
   console.log(chartRow.children[0].children);
   console.log(chartRow.children[0].children[0].className);
-  // input is [1, 5, 3, 5, 1]
-  // output is 
-    // 1: [1, 2, 0] 
-    // 2: [2, 4, 2] If output[key][2] > 0, add blue (after any input[i])
-    // 4: [4, 5, 0]
-    // most: [2, 4, 2] If key === most, output[key][0] and output[key][1], black blocks
-
-  // Add blue blocks
-  // Iterate through hash
-  // If output[key][2] > 0, add blue (after any input[i])
-    // Iterate through hash[key]
-    // hash[key][0] = leftWall -1
-    // hash[key][1] = rightWall-1
 
   for (let key in output) {
     console.log(output[key]);
@@ -62,25 +49,14 @@ const renderChart = (input, output) => {
 
         let numOfBlueBlocks = input[shorterWall] - input[i];
         console.log('numOfBlueBlocks is ', numOfBlueBlocks);
-        // input[3] - input[2]
-        // 5 - 3
-        // numo0fBlueBlocks = 2
 
-        // i is the column/wall we want to add blue blocks to
-
-        // Iterate through td's children...
-
-        console.log('maxHeight is ', maxHeight);
         for (let j = 0; j < maxHeight; j++) {
-          console.log('j is', j);
+          console.log('j is', j); // 0 1 2 3 4 5...
 
-          // if (j === 1) {
-          //   chartRow.children[i].children[j].className += ' blue-block';
-          // }
           console.log('input[i]', input[i]);
 
 
-          if (j < input[i] && j >= input[i] - numOfBlueBlocks) {
+          if (j < (maxHeight - input[i]) && (j >= maxHeight - (input[i] + numOfBlueBlocks))) {
             console.log('I am inside if statement');
             console.log(chartRow.children[i].children[j].className);
             chartRow.children[i].children[j].className += ' blue-block';
@@ -91,8 +67,6 @@ const renderChart = (input, output) => {
       }
     }
   }
-
-  // when output is []
 
   chart.appendChild(chartRow);
   tableContainer.appendChild(chart);
