649f6c25b0572b7c2f6c78e4fa4449c39e283a87
Lisa Gee <zhusufeng@gmail.com>
2018-01-28 18:40:49 -0800

    Remove excess console.logs from renderChart

--- client/view/renderChart.js
+++ client/view/renderChart.js
@@ -5,7 +5,7 @@ const renderChart = (input, output) => {
       tableContainer.removeChild(tableContainer.firstChild);
   }
 
-  // if input is []
+  // TODO: account for when input is []
 
   const maxHeight = findMaxHeight(input) + 1;
   console.log('This is height of our chart', maxHeight);
@@ -26,46 +26,25 @@ const renderChart = (input, output) => {
       if (input[i] >= j) {
         block.className += ' gray-block';
       }
-
       cell.appendChild(block);
     }
-
     chartRow.appendChild(cell);
   }
 
-  console.log(chartRow.children[0].children);
-  console.log(chartRow.children[0].children[0].className);
 
   for (let key in output) {
     console.log(output[key]);
     if (output[key][2] > 0 && key !== 'most') {
-      console.log('i am inside ');
       let shorterWall = (input[output[key][0] - 1] < input[output[key][1] - 1]) ? output[key][0] - 1 : output[key][1] - 1;
-      console.log('shorterWall is index ', shorterWall);
       for (let i = output[key][0]; i < output[key][1] - 1; i++) {
-        console.log('i is', i);
-        console.log('input[shorterWall] is', input[shorterWall]);
-        console.log('input[i] is', input[i]);
-
         let numOfBlueBlocks = input[shorterWall] - input[i];
-        console.log('numOfBlueBlocks is ', numOfBlueBlocks);
-
         let bottomIndex = maxHeight - input[i];
         let topIndex = maxHeight - (input[i] + numOfBlueBlocks);
 
-        console.log(`bottomIndex: ${bottomIndex} and topIndex: ${topIndex}`);
-
         for (let j = 0; j < maxHeight; j++) {
-          console.log('j is', j); // 0 1 2 3 4 5...
-          console.log('input[i]', input[i]);
-
           if (j < bottomIndex && j >= topIndex){
-            console.log('I am inside if statement');
-            console.log(chartRow.children[i].children[j].className);
             chartRow.children[i].children[j].className += ' blue-block';
           }
-
-          // needs to be colored when j is 1 and 2
         }
       }
     }
